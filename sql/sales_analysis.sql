8fd74dc26f69e45ac2f4f63ef81aad53f4119fe6
Your Name <youremail@example.com>
2025-10-01 17:59:08 -0400

    Your commit message here

--- sql/sales_analysis.sql
+++
@@ -1,8 +0,0 @@
-git add sql/sales_analysis.sql git commit -m 
-"Add SQL analysis scripts" git push origin 
-main
-mkdir notebooks
-git add notebooks/
-git commit -m "Add notebooks folder"
-git push origin main
-
